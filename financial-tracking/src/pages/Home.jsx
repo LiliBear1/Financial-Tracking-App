e3e0923dbf15d864299a6188aeaa1208c1aed6bf
Shannon-Blade <shannoninengland@gmail.com>
2023-02-16 12:42:12 +0000

    added income card with input fields

--- financial-tracking/src/pages/Home.jsx
+++ financial-tracking/src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Income from './Income';
+import Expenses from './Expenses';
+import Savings from './Savings';
+import { ChakraProvider } from '@chakra-ui/react';
+
+
 function Home() {
     return (
         <div>
+        <ChakraProvider>
             <p>Home world</p>
+        <Income />
+        <Expenses />
+        <Savings />
+        </ChakraProvider>
         </div>
     )
 }
